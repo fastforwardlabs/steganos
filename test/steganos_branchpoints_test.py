5441b9d6fe8559726b1a715e783d8c872dc08791
Noam Finkelstein <noam@avaaz.org>
2015-12-20 20:52:44 -0500

    remove branchpoints argument

--- test/steganos_branchpoints_test.py
+++ test/steganos_branchpoints_test.py
@@ -5,7 +5,7 @@ from ..src import steganos
 # @unittest.mock.patch('steganos.src.steganos.get_all_branchpoints')
 class TestSteganosBranchpoints(object):
 
-    def test_encode(self, branchpoints):
+    def test_encode(self):
         # given
         text = '"I am 9," he said.'
         bits = '01'
@@ -16,7 +16,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert result == '"I am nine," he said\u200f\u200e.'
 
-    def test_encode_a_single_bit(self, branchpoints):
+    def test_encode_a_single_bit(self):
         # given
         text = '"I am 9," he said.'
         bits = '1'
@@ -27,9 +27,9 @@ class TestSteganosBranchpoints(object):
         # then
         assert result == "'I\u0083 am nine,' he said\u200f\u200e."
 
-    def test_encode_raises_when_given_too_many_bits_for_text(self, branchpoints):
+    def test_encode_raises_when_given_too_many_bits_for_text(self):
         # given
-        text = '9 and some more text'
+        text = 'x'
         bits = '110'
 
         # then
@@ -39,7 +39,7 @@ class TestSteganosBranchpoints(object):
         assert (str(steganos.bit_capacity(text)) in str(execinfo.value)
                 and str(len(bits)) in str(execinfo.value))
 
-    def test_decode(self, branchpoints):
+    def test_decode(self):
         # given
         text = '"I am 9." he said.'
         bits = '01'
@@ -51,7 +51,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert bits in result
 
-    def test_decode_a_single_bit(self, branchpoints):
+    def test_decode_a_single_bit(self):
         # given
         text = '"I am 9." he said.'
         bits = '1'
@@ -63,7 +63,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert '1111' in result
 
-    def test_decode_with_bad_origin(self, branchpoints):
+    def test_decode_with_bad_origin(self):
         # given
         original_text = 'This is a sentence with a 9.'
         encoded_text = 'This does not match with a 9.'
@@ -72,7 +72,7 @@ class TestSteganosBranchpoints(object):
         with pytest.raises(ValueError):
             steganos.decode_full_text(encoded_text, original_text)
 
-    def test_encoding_when_digits_appear_before_quotes(self, branchpoints):
+    def test_encoding_when_digits_appear_before_quotes(self):
             # given
             text = 'Chapter 9 - "Hello!"'
             bits = '10'
@@ -84,7 +84,7 @@ class TestSteganosBranchpoints(object):
             # then
             assert bits in result
 
-    def test_get_indices_of_encoded_text_when_text_is_at_start(self, branchpoints):
+    def test_get_indices_of_encoded_text_when_text_is_at_start(self):
         # given
         text = 'Chapter 9 - "Hello!", Chapter 10 - "Goodbye!"'
         encoded_text = "Chapter nine - 'Hello!'"
@@ -95,7 +95,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert result == (0, 20)
 
-    def test_get_indices_when_encoded_text_not_at_start(self, branchpoints):
+    def test_get_indices_when_encoded_text_not_at_start(self):
         # given
         text = 'Chapter 9 - "Hello!", Chapter 8 - "Goodbye!"'
         encoded_text = 'nine - "Hello!", Chapter eight'
@@ -106,7 +106,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert result == (8, 31)
 
-    def test_get_indices_when_change_is_not_start_of_encoded_text(self, branchpoints):
+    def test_get_indices_when_change_is_not_start_of_encoded_text(self):
         # given
         text = 'Chapter 9 - "Hello!", Chapter 10 - "Goodbye!"'
         encoded_text = " - 'Hello!', Chapter "
@@ -117,7 +117,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert result == (9, 30)
 
-    def test_get_indices_when_end_is_mid_change_in_encoded(self, branchpoints):
+    def test_get_indices_when_end_is_mid_change_in_encoded(self):
         # given
         text = 'Chapter 9 - "Hello!", Chapter 8 - "Goodbye!"'
         encoded_text = " - 'Hello!', Chapter eig"
@@ -128,7 +128,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert result == (9, 31)
 
-    def test_get_indices_when_start_is_mid_change_in_original(self, branchpoints):
+    def test_get_indices_when_start_is_mid_change_in_original(self):
         # given
         text = "I won't do it."
         encoded_text = "not do it\u200f\u200e."
@@ -139,7 +139,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert result == (3, 14)
 
-    def test_get_indices_when_end_is_mid_change_in_original(self, branchpoints):
+    def test_get_indices_when_end_is_mid_change_in_original(self):
         # given
         text = "I won't."
         encoded_text = "I will"
@@ -150,7 +150,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert result == (0, 6)
 
-    def test_get_indices_when_start_is_mid_unchanged_change_in_original(self, branchpoints):
+    def test_get_indices_when_start_is_mid_unchanged_change_in_original(self):
         # given
         text = "I won't turn 9"
         encoded_text = "n't turn nine"
@@ -161,7 +161,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert result == (4, 14)
 
-    def test_get_indices_when_end_is_mid_unchanged_change_in_original(self, branchpoints):
+    def test_get_indices_when_end_is_mid_unchanged_change_in_original(self):
         # given
         text = "I won't turn 9."
         encoded_text = "I wo"
@@ -172,7 +172,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert result == (0, 4)
 
-    def test_when_global_change_out_of_encoded_text(self, branchpoints):
+    def test_when_global_change_out_of_encoded_text(self):
         # given
         text = 'I am 9, but I say "I am 8".'
         encoded_text = steganos.encode('11', text)
@@ -183,7 +183,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert '?1' in result
 
-    def test_local_changes_appear_after_global_changes_in_decoded_bits(self, branchpoints):
+    def test_local_changes_appear_after_global_changes_in_decoded_bits(self):
         # given
         text = 'I am 9\t, but I say "I am 8".'
         encoded_text = steganos.encode('111', text)
@@ -194,7 +194,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert '?11' in result
 
-    def test_global_change_late_in_encoded_text_with_negative_indices(self, branchpoints):
+    def test_global_change_late_in_encoded_text_with_negative_indices(self):
         # given
         text = 'I am 9\t, but I say "I am 8".'
         encoded_text = steganos.encode('111', text)
@@ -205,7 +205,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert '11?' in result
 
-    def test_global_change_late_in_encoded_text(self, branchpoints):
+    def test_global_change_late_in_encoded_text(self):
         # given
         text = 'I am 9\t, but I say "I am 8"'
         encoded_text = steganos.encode('111', text)
@@ -216,7 +216,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert '11?' in result
 
-    def test_bit_capacity(self, branchpoints):
+    def test_bit_capacity(self):
         # given
         text = 'I am 9\t, but I say "I am 8."'
 
@@ -226,7 +226,7 @@ class TestSteganosBranchpoints(object):
         # then
         assert result >= 3
 
-    def test_get_all_branchpoints_finds_matching_quotes(self, branchpoints):
+    def test_get_all_branchpoints_finds_matching_quotes(self):
         # given
         text = '"Hello," he said.'
 
