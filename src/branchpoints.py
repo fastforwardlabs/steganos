2e8e2f72b3267ce046f0de1217a13a8d8f6a9aad
Noam Finkelstein <noam@avaaz.org>
2015-12-21 03:44:13 -0500

    consolidate branchpoints

--- src/branchpoints.py
+++ src/branchpoints.py
@@ -1,14 +1,63 @@
-from . import local_branchpoints
-from . import global_branchpoints
-from . import unicode_branchpoints
+import re
 
 def get_all_branchpoints(text: str):
     # local and unicode branchpoints are sorted to maximize the information that can be
     # retrieved from any contiguous piece of encoded text
-    sorted_branchpoints = sort_branchpoints(local_branchpoints.get_all(text) + unicode_branchpoints.get_all(text))
-    branchpoints = global_branchpoints.get_all(text) + sorted_branchpoints
+    sorted_branchpoints = sort_branchpoints(ascii_branchpoints(text) + unicode_branchpoints(text))
+    branchpoints = global_branchpoints(text) + sorted_branchpoints
     return remove_redundant_characters(text, branchpoints)
 
+def ascii_branchpoints(text: str):
+    return get_tab_branchpoints(text) + get_contraction_branchpoints(text)
+
+def unicode_branchpoints(text: str):
+    return get_directional_mark_branchpoints(text) + get_non_breaking_branchpoints(text)
+
+def global_branchpoints(text: str):
+    global_branchpoints = [get_single_quotes_branchpoint(text), get_single_digit_branchpoint(text)]
+    return [bp for bp in global_branchpoints if bp]
+
+def get_tab_branchpoints(text: str):
+    tab_indices = [m.start() for m in re.finditer('\t', text)]
+    return [[(tab_index, tab_index + 1, '    ')] for tab_index in tab_indices]
+
+def get_contraction_branchpoints(text: str):
+    contractions = {
+        "won't": 'will not'
+    }
+    branchpoints = []
+    for contraction, long_form in contractions.items():
+       index = text.find(contraction)
+       if index > -1:
+           branchpoints.append([(index, index + len(contraction), long_form)])
+    return branchpoints
+
+def get_single_quotes_branchpoint(text: str):
+    double_quote_indices = [m.start() for m in re.finditer('"', text)]
+    return [(index, index + 1, "'") for index in double_quote_indices]
+
+def get_single_digit_branchpoint(text: str):
+    numbers = {
+            '9': 'nine',
+            '8': 'eight',
+            '7': 'seven',
+            '6': 'six',
+            '5': 'five',
+            '4': 'four',
+            '3': 'three',
+            '2': 'two',
+            '1': 'one'
+    }
+    single_digit_indices = [m.start() for m in re.finditer('(?<![\d\.])[1-9](?![\d\.])', text)]
+    return [(index, index + 1, numbers[text[index]]) for index in single_digit_indices]
+
+def get_directional_mark_branchpoints(text: str):
+   period_indices = [index for index, char in enumerate(text) if char == '.']
+   return [[(index, index, '\u200f\u200e')] for index in period_indices]
+
+def get_non_breaking_branchpoints(text: str):
+    capital_letter_indices = [index for index, char in enumerate(text) if char.isupper()]
+    return [[(index + 1, index + 1, '\u0083')] for index in capital_letter_indices]
 
 def remove_redundant_characters(original_text: str, branchpoints: list):
     """
@@ -62,3 +111,4 @@ def sort_branchpoints(branchpoints: list):
     branchpoints.sort(key=first_change)
 
     return branchpoints
+
--- test/branchpoints_test.py
+++ test/branchpoints_test.py
@@ -1,5 +1,5 @@
-from ..src.branchpoints import remove_redundant_characters
-from ..src.branchpoints import get_all_branchpoints
+import pytest
+from ..src.branchpoints import *
 
 def test_remove_single_character_prefix_and_suffix_for_change():
     # given
@@ -43,4 +43,74 @@ def test_get_all_branchpoints_finds_matching_quotes():
 
     # then
     assert [(0, 1, "'"), (7, 8, "'")] in result
+
+@pytest.mark.parametrize('text, branchpoints', [
+    ('\tHello.', [[(0, 1, '    ')]]),
+    ('\tHello.\t', [[(0, 1, '    ')], [(7, 8, '    ')]]),
+    ('No tabs!', [])
+])
+def test_get_tab_branchpoints(text, branchpoints):
+    # when
+    result = get_tab_branchpoints(text)
+
+    # then
+    assert result == branchpoints
+
+@pytest.mark.parametrize('text, branchpoints', [
+    ("I won't do it.", [[(2, 7, 'will not')]])
+])
+def test_get_contraction_branchpoints(text, branchpoints):
+    # when
+    result = get_contraction_branchpoints(text)
+
+    # then
+    assert result == branchpoints
+
+@pytest.mark.parametrize('text, branchpoint', [
+    ('"A" and "B"', [(0, 1, "'"), (2, 3, "'"), (8, 9, "'"), (10, 11, "'")]),
+    ('There are no quotes in this text.', [])
+])
+def test_get_global_single_quotes_branchpoint(text, branchpoint):
+    # when
+    result = get_single_quotes_branchpoint(text)
+
+    # then
+    assert result == branchpoint
+
+@pytest.mark.parametrize('text, branchpoint', [
+    ('He was 9', [(7, 8, 'nine')]),
+    ('He is not 88, he is 8, he says.', [(20, 21, 'eight')]),
+    ('7, 8, 9', [(0, 1, 'seven'), (3, 4, 'eight'), (6, 7, 'nine')]),
+    ('There are no numbers in this text.', [])
+])
+def test_get_global_single_digit_branchpoint(text, branchpoint):
+    # when
+    result = get_single_digit_branchpoint(text)
+
+    # then
+    assert result == branchpoint
+
+@pytest.mark.parametrize('text, branchpoints', [
+    ('Hello. I am sam.', [[(5, 5, '\u200f\u200e')], [(15, 15, '\u200f\u200e')]]),
+    ('Period.', [[(6, 6, '\u200f\u200e')]]),
+    ('No periods!', [])
+])
+def test_diretional_mark_branchpoints(text, branchpoints):
+    # when
+    result = get_directional_mark_branchpoints(text)
+
+    # then
+    assert result == branchpoints
+
+@pytest.mark.parametrize('text, branchpoints', [
+    ('Hello. I am sam.', [[(1, 1, '\u0083')], [(8, 8, '\u0083')]]),
+    ('Capital.', [[(1, 1, '\u0083')]]),
+    ('no caps!', [])
+])
+def test_non_breaking_branchpoints(text, branchpoints):
+    # when
+    result = get_non_breaking_branchpoints(text)
+
+    # then
+    assert result == branchpoints
 
